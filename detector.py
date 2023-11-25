fbeb72e157cffa648120db56c68053aa26234571
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-11-26 00:29:52 +0200

    Update detector.py

--- detector.py
+++ detector.py
@@ -3,10 +3,11 @@ from concurrent.futures import ThreadPoolExecutor
 from collections import defaultdict
 from urllib.parse import urlparse
 import argparse
+import sys
 
 def make_request(url, domain_states, protocol, output_file):
     if domain_states[url]['unreachable']:
-        output_file.write(f"Skipping checks for unreachable domain: {url}\n")
+        output(f"Skipping checks for unreachable domain: {url}", output_file)
         return
 
     payloads = ["%20or%201=1--", "%20or%201=2--"]
@@ -23,21 +24,26 @@ def make_request(url, domain_states, protocol, output_file):
 
                 if index > 1:
                     if response.status_code != responses[index - 2].status_code:
-                        output_file.write(f"Status code changed for URL: {url}\n")
+                        output(f"Status code changed for URL: {url}", output_file)
                     if response.text != responses[index - 2].text:
-                        output_file.write(f"Response body changed for URL: {url}\n")
+                        output(f"Response body changed for URL: {url}", output_file)
 
         except requests.exceptions.RequestException:
-            # Do nothing when connection fails
             domain_states[url]['unreachable'] = True
             break
+
+def output(message, output_file):
+    if output_file:
+        output_file.write(message + "\n")
+    else:
+        print(message)
 
 def main():
     parser = argparse.ArgumentParser(description='SQL injection vulnerability checker')
     parser.add_argument('-t', '--targets', type=str, help='File containing URLs to check', required=True)
     parser.add_argument('-c', '--threads', type=int, help='Number of threads', default=5)
     parser.add_argument('-p', '--protocol', type=str, choices=['http', 'https'], help='HTTP protocol', default='https')
-    parser.add_argument('-o', '--output', type=str, help='Output file name', default='output.txt')
+    parser.add_argument('-o', '--output', type=str, help='Output file name')
 
     args = parser.parse_args()
 
@@ -46,9 +52,16 @@ def main():
 
     domain_states = defaultdict(lambda: {'unreachable': False})
 
-    with open(args.output, 'w') as output_file, ThreadPoolExecutor(max_workers=args.threads) as executor:
+    output_file = None
+    if args.output:
+        output_file = open(args.output, 'w')
+
+    with ThreadPoolExecutor(max_workers=args.threads) as executor:
         for url in urls:
             executor.submit(make_request, url, domain_states, args.protocol, output_file)
+
+    if output_file:
+        output_file.close()
 
 if __name__ == "__main__":
     main()
