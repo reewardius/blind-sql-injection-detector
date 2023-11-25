27fa3e5f6f36323f3357e7a27d2692b28fb1359c
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-11-26 00:20:59 +0200

    Update detector.py

--- detector.py
+++ detector.py
@@ -4,49 +4,51 @@ from collections import defaultdict
 from urllib.parse import urlparse
 import argparse
 
-def make_request(url, domain_states, protocol):
+def make_request(url, domain_states, protocol, output_file):
     if domain_states[url]['unreachable']:
-        print(f"Skipping checks for unreachable domain: {url}")
+        output_file.write(f"Skipping checks for unreachable domain: {url}\n")
         return
 
-    payloads = ["%20or%201=1--", "%20or%201=2--"]  # Ваши SQL-инъекции
+    payloads = ["%20or%201=1--", "%20or%201=2--"]
     responses = []
 
     for index, payload in enumerate(payloads, start=1):
         parsed_url = urlparse(url)
-        new_url = url if parsed_url.scheme else f"{protocol}://{url}"  # Добавление протокола, если его нет в URL
+        new_url = url if parsed_url.scheme else f"{protocol}://{url}"
 
         try:
-            response = requests.get(f"{new_url}" + payload)
-            responses.append(response)
+            with requests.Session() as session:
+                response = session.get(f"{new_url}" + payload)
+                responses.append(response)
 
-            if index > 1:
-                if response.status_code != responses[index - 2].status_code:
-                    print(f"Status code changed for URL: {url}")
-                if response.text != responses[index - 2].text:
-                    print(f"Response body changed for URL: {url}")
+                if index > 1:
+                    if response.status_code != responses[index - 2].status_code:
+                        output_file.write(f"Status code changed for URL: {url}\n")
+                    if response.text != responses[index - 2].text:
+                        output_file.write(f"Response body changed for URL: {url}\n")
 
         except requests.exceptions.RequestException:
-            print(f"Failed to connect or resolve for URL: {url}. Skipping checks...")
+            # Do nothing when connection fails
             domain_states[url]['unreachable'] = True
-            break  # Прекратить проверки для этого домена
+            break
 
 def main():
     parser = argparse.ArgumentParser(description='SQL injection vulnerability checker')
     parser.add_argument('-t', '--targets', type=str, help='File containing URLs to check', required=True)
     parser.add_argument('-c', '--threads', type=int, help='Number of threads', default=5)
     parser.add_argument('-p', '--protocol', type=str, choices=['http', 'https'], help='HTTP protocol', default='https')
+    parser.add_argument('-o', '--output', type=str, help='Output file name', default='output.txt')
 
     args = parser.parse_args()
 
     with open(args.targets, 'r') as file:
-        urls = file.readlines()
-        urls = [url.strip() for url in urls]
+        urls = [url.strip() for url in file.readlines()]
 
     domain_states = defaultdict(lambda: {'unreachable': False})
 
-    with ThreadPoolExecutor(max_workers=args.threads) as executor:
-        executor.map(make_request, urls, [domain_states] * len(urls), [args.protocol] * len(urls))
+    with open(args.output, 'w') as output_file, ThreadPoolExecutor(max_workers=args.threads) as executor:
+        for url in urls:
+            executor.submit(make_request, url, domain_states, args.protocol, output_file)
 
 if __name__ == "__main__":
     main()
